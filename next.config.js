ee1b3d37acac7cde8c483b4bb698a61d1eebd06c
Toowiredd <155716379+Toowiredd@users.noreply.github.com>
2024-04-28 11:26:09 +1000

    improve

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['example.com'], // add the domains from where you want to load images
+  },
+  webpack: (config, { isServer }) => {
+    // Fix issues with file-loader, url-loader and esbuild
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        os: false,
+        crypto: false,
+      };
+    }
+    return config;
+  },
+}
 
 module.exports = nextConfig
+
